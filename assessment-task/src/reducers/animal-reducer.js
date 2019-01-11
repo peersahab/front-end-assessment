baa48b6d12d02af0e57347548175479e80045314
Peer Sahab <30799360+peersahab@users.noreply.github.com>
2019-01-11 11:20:49 +0500

    Creating app again using webpack

--- assessment-task/src/reducers/animal-reducer.js
+++
@@ -1,9 +0,0 @@
-import { STORE_ANIMALS } from "../actions/animal-actions";
-export default function animalReducer(state = '', {type, payload}){
-    switch (type){
-        case STORE_ANIMALS:
-            return payload.animalsList;
-        default:
-        return state;
-    }
-}
--- assessment-task/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import animalReducer from './animal-reducer';
-const comReducer = combineReducers({
-animalReducer: animalReducer
-});
-
-export default comReducer;
